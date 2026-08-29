da16a3bb3d98677e3014e7057da32be24189df7e
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-08-29 16:03:19 +0300

    Specifications

---
+++ HotelBooking.Domain/Common/ISpecifications.cs
@@ -0,0 +1,11 @@
+﻿using HotelBooking.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace HotelBooking.Domain.Common
+{
+    interface ISpecifications<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        ICollection<Expression<Func<TEntity, object>>> IncludeExpressions { get; }
+        //Condition , OrderBy , Pagination
+    }
+}
