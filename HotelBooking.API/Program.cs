884961e62f26639818523eeca3143bd6fa79f125
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-08-30 06:35:48 +0300

    GlobalExeptionMiddleware

--- HotelBooking.API/Program.cs
+++ HotelBooking.API/Program.cs
@@ -1,9 +1,12 @@
+using HotelBooking.API.Middlewares;
 using HotelBooking.Domain.Common;
 using HotelBooking.Infrastructure;
 using HotelBooking.UseCases;
-using Microsoft.EntityFrameworkCore.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddExceptionHandler<GlobalExeptionMiddleware>();
+builder.Services.AddProblemDetails();
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -13,6 +16,7 @@ builder.Services.AddInfastructureServices(builder.Configuration);
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
+app.UseExceptionHandler();
 
 using var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredKeyedService<IDataSeeder>("hotel");
